f04d922457def17cb6944e9a6b284e2636b04c16
CaptainB <bin@fit2cloud.com>
2022-04-21 15:18:55 +0800

    perf: 优化查询子类的方法

--- src/main/java/io/metersphere/streaming/report/ReportGeneratorFactory.java
+++ src/main/java/io/metersphere/streaming/report/ReportGeneratorFactory.java
@@ -2,6 +2,7 @@ package io.metersphere.streaming.report;
 
 import io.metersphere.streaming.commons.utils.LogUtil;
 import io.metersphere.streaming.report.impl.AbstractReport;
+import org.apache.commons.collections4.CollectionUtils;
 import org.reflections.Reflections;
 
 import java.util.ArrayList;
@@ -9,19 +10,24 @@ import java.util.List;
 import java.util.Set;
 
 public class ReportGeneratorFactory {
+    private static List<AbstractReport> reportGenerators = new ArrayList<>();
 
-    public static List<AbstractReport> getReportGenerators() {
+    public synchronized static List<AbstractReport> getReportGenerators() {
+        if (CollectionUtils.isNotEmpty(reportGenerators)) {
+            return reportGenerators;
+        }
+
         Reflections reflections = new Reflections(ReportGeneratorFactory.class);
         Set<Class<? extends AbstractReport>> subTypes = reflections.getSubTypesOf(AbstractReport.class);
-        List<AbstractReport> reportGenerators = new ArrayList<>();
+        List<AbstractReport> result = new ArrayList<>();
         subTypes.forEach(s -> {
             try {
-                reportGenerators.add(s.getDeclaredConstructor().newInstance());
+                result.add(s.getDeclaredConstructor().newInstance());
             } catch (Exception e) {
                 LogUtil.error(e);
             }
         });
-
+        reportGenerators = result;
         return reportGenerators;
     }
 }
